75677a5e5ce0b6a24e19a395f75dcfb2dd652eb3
bharath sn <bharathsn0812@gmail.com>
2019-09-29 01:58:16 +0530

    video playlist item update

--- src/app/tutor-playlist-video-edit/tutor-playlist-video-edit.component.ts
+++ src/app/tutor-playlist-video-edit/tutor-playlist-video-edit.component.ts
@@ -35,6 +35,9 @@ export class TutorPlaylistVideoEditComponent implements OnInit {
   videoTitle='';
   videoLink='';
   disableFields = true;
+  CourseId: any;
+  playlistId: any;
+  disableFieldsVideo:any;
 
   // constructor() { }
   constructor(private router: Router, private storage: AngularFireStorage,private cookieService: CookieService, 
@@ -45,20 +48,100 @@ export class TutorPlaylistVideoEditComponent implements OnInit {
     private http: HttpClient,
     private activatedRoute: ActivatedRoute) { }
 
-  ngOnInit() {
-      // this.http.get<any>('https://geekcharge.firebaseapp.com/api/v1/tutor/course/'+this.CourseId,requestOptions)
-      // .subscribe 
-      //   (data => {
-      //   },
-      //  (error: any) => {
-      //    console.log(error.error);
-      //    if (error.error === 'unauthorized'){
-      //     this.cookieService.delete('__session');
-      //     this.cookieService.delete('__profilepic');
+  cancelEdit (event){
+    this.router.navigateByUrl('/corsefeed');
+  }
 
-      //     this.router.navigateByUrl('/login');
-      //    }
-      //  }    
+  uploadVideo(event) {
+
+    console.log('upload files');
+
+    this.disableFieldsVideo = true;
+    const file = event.target.files[0];
+    const filePath = '/video/'+Math.floor(Date.now())+'_'+file.name;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+
+    // observe percentage changes
+    this.uploadPercent = task.percentageChanges();
+    // get notified when the download URL is available
+    task.snapshotChanges().pipe(
+        finalize(() =>{
+
+          // when this function is executed
+          var getDownloadURL1 = fileRef.getDownloadURL();
+          getDownloadURL1.subscribe(url => {
+            console.log(url);
+            this.videoLink = url;
+            this.disableFieldsVideo = false;
+          });
+        })
+        )
+    .subscribe()
+  }  
+
+  updateCourse(event: Event){
+
+        let videoData = {"playlistType":'video', "videoTitle": this.videoTitle,  "videoLink": this.downloadURL1};
+
+        this.idToken = this.cookieService.get('__session');
+        console.log(this.idToken);
+        let idTokenBearer =  'Bearer '+this.idToken;
+        const requestOptions = {                                                                                                                                                                                 
+          headers: new HttpHeaders({'Authorization': idTokenBearer})
+        };
+      
+    
+        this.http.patch('https://geekcharge.firebaseapp.com/api/v1/tutor/course/'+this.CourseId+'/'+this.playlistId,videoData, requestOptions)
+        .subscribe 
+        (data => {
+          console.log(data);
+          this.router.navigateByUrl('/corsefeed');  
+        },
+        (error: any) => {
+          console.log(error);
+          
+          if (error.error === 'unauthorized'){
+            this.cookieService.delete('__session');
+            this.cookieService.delete('__profilepic');
+            this.router.navigateByUrl('/login');
+          } else {
+            this.router.navigateByUrl('/corsefeed');
+          }
+
+        }
+        )
+    }  
+
+  ngOnInit() {
+
+    this.playlistId = this.activatedRoute.snapshot.queryParams["playlistId"];
+    this.CourseId = this.cookieService.get('__CourseId');
+    this.idToken = this.cookieService.get('__session');
+    console.log(this.idToken);
+    let idTokenBearer =  'Bearer '+this.idToken;
+
+    const requestOptions = {                                                                                                                                                                                 
+      headers: new HttpHeaders({'Authorization': idTokenBearer})
+    };
+
+    this.http.get<any>('https://geekcharge.firebaseapp.com/api/v1/tutor/course/'+this.CourseId+'/'+this.playlistId,requestOptions)
+    .subscribe 
+      (data => {
+        console.log(data);
+        this.videoTitle = data.videoTitle;
+        this.videoLink = data.videoLink;        
+      }),
+     (error: any) => {
+       console.log(error.error);
+       if (error.error === 'unauthorized'){
+        this.cookieService.delete('__session');
+        this.cookieService.delete('__profilepic');
+
+        this.router.navigateByUrl('/login');
+       }
+     }    
+
   }
 
 }
